ed8697b51ab75fc1bbca385a33e5b84d1e1abd01
Vicente Losada <vi_losada@hotmail.com>
2024-04-02 23:06:21 +0200

    Create header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Suspense } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 import "./globals.css";
 
@@ -16,7 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Suspense>{children}</Suspense>
+        <Suspense>
+          <header className="navbar bg-neutral">
+            <Link href="/" title="Dragon Ball">
+              <Image src="/logo.png" alt="" width={100} height={26} />
+            </Link>
+          </header>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
